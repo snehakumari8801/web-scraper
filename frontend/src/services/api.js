a94696acdc37d5511148c50b7f84e28abf00e137
snehakumari8801 <snehakumari8801@gmail.com>
2026-05-08 16:58:14 +0530

    add url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://web-scraper-backend3.onrender.com/api"
 });
 
 export const register = (data) => API.post("/auth/register", data);
