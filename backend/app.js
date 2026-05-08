d5e482c7cbdbd03f769d42562d70015b6be2edfb
snehakumari8801 <snehakumari8801@gmail.com>
2026-05-08 17:09:42 +0530

    allow origin

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,9 @@ const scraperRoutes = require("./routes/scraperRoutes");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+
+app.use(cors());
+
 app.use(express.json());
 app.use("/api", scraperRoutes);
 
